c70fa2cd2a11d9e0f426bdcfb3394f9a0ea934e0
nklloyd <nicholask.lloyd@gmail.com>
2025-01-25 00:20:51 -0500

    Initial Setup

--- courselynx/app/(tabs)/index.tsx
+++ courselynx/app/(tabs)/index.tsx
@@ -11,13 +11,12 @@ export default function HomeScreen() {
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={require('@/assets/images/ai.png')}
           style={styles.reactLogo}
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
+        <ThemedText type="title">Courselynx</ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
