11ebc9b76fa7dccb909faf079fd7d9c05ffe497d
patwu22 <wu.patrick22@gmail.com>
2025-02-24 15:08:23 -0500

    Add InputField component

--- courselynx/app/settings/profile.tsx
+++ courselynx/app/settings/profile.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { View, Text, TextInput, Image, TouchableOpacity, StyleSheet, ScrollView } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import { InputField } from "@/components/InputField";
 
 export default function ProfileSelf() {
   // State for form inputs
@@ -11,58 +12,37 @@ export default function ProfileSelf() {
   const [linkedin, setLinkedin] = useState("abcxyz");
   const [discord, setDiscord] = useState("");
 
+  function saveProfile() {
+    console.log("Save");
+  }
+
   return (
-    <ScrollView contentContainerStyle={styles.container}>
+    <ScrollView contentContainerStyle={styles.container} automaticallyAdjustKeyboardInsets={true}>
       {/* Profile Image */}
       <View style={styles.profileContainer}>
-        <Image source={{ uri: "https://via.placeholder.com/150" }} style={styles.profileImage} />
+        <Image source={{ uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Bohol_-_Chocolate_Hills.jpg/2560px-Bohol_-_Chocolate_Hills.jpg" }} style={styles.profileImage} />
         <TouchableOpacity style={styles.editButton}>
-          <Ionicons name="pencil" size={16} color="white" />
+          <Ionicons name="pencil" size={18} color="white" />
         </TouchableOpacity>
       </View>
 
       {/* Input Fields */}
       <View style={styles.form}>
-        <Text style={styles.label}>Name*</Text>
-        <TextInput style={styles.input} value={name} onChangeText={setName} />
+        <InputField label="Name*" value={name} onChangeText={setName} placeholder="Type your name" />
 
-        <Text style={styles.label}>Major*</Text>
-        <TextInput style={styles.input} value={major} onChangeText={setMajor} />
+        <InputField label="Major*" value={major} onChangeText={setMajor} placeholder="Type your major" />
 
-        <Text style={styles.label}>Graduation Year*</Text>
-        <TextInput style={styles.input} value={gradYear} onChangeText={setGradYear} keyboardType="numeric" />
+        <InputField label="Graduation Year*" value={gradYear} onChangeText={setGradYear} placeholder="Type your graduation year" keyboardType="numeric" />
 
-        <Text style={styles.label}>Bio</Text>
-        <TextInput
-          style={[styles.input, styles.textArea]}
-          value={bio}
-          onChangeText={setBio}
-          placeholder="Tell us about yourself"
-          placeholderTextColor={"#888"}
-          multiline
-        />
+        <InputField label="Bio" value={bio} onChangeText={setBio} placeholder="Tell us about yourself" />
 
-        <Text style={styles.label}>LinkedIn</Text>
-        <TextInput
-          style={styles.input}
-          value={linkedin}
-          onChangeText={setLinkedin}
-          placeholder="Type your username"
-          placeholderTextColor={"#888"}
-        />
+        <InputField label="LinkedIn" value={linkedin} onChangeText={setLinkedin} placeholder="Type your username" />
 
-        <Text style={styles.label}>Discord</Text>
-        <TextInput
-          style={styles.input}
-          value={discord}
-          onChangeText={setDiscord} 
-          placeholder="Type your username"
-          placeholderTextColor={"#888"}
-        />
+        <InputField label="Discord" value={discord} onChangeText={setDiscord} placeholder="Type your username" />
       </View>
 
       {/* Save Button */}
-      <TouchableOpacity style={styles.saveButton}>
+      <TouchableOpacity style={styles.saveButton} onPress={saveProfile}>
         <Text style={styles.saveButtonText}>Save</Text>
       </TouchableOpacity>
     </ScrollView>
@@ -73,47 +53,33 @@ const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
     padding: 20,
+    paddingTop: 5,
     backgroundColor: "white",
   },
   profileContainer: {
     alignItems: "center",
-    marginBottom: 20,
+    marginBottom: 10,
   },
   profileImage: {
-    width: 100,
-    height: 100,
-    borderRadius: 50,
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+  },
+  form: {
+    marginBottom: 10,
   },
   editButton: {
     position: "absolute",
     bottom: 5,
-    right: 5,
+    left: "50%",
+    transform: [{ translateX: 45 }],
     backgroundColor: "#3b82f6",
-    width: 24,
-    height: 24,
-    borderRadius: 12,
+    width: 30,
+    height: 30,
+    borderRadius: 15,
     alignItems: "center",
     justifyContent: "center",
-  },
-  form: {
-    marginBottom: 20,
-  },
-  label: {
-    fontSize: 14,
-    fontWeight: "500",
-    marginBottom: 5,
-    color: "#555",
-  },
-  input: {
-    backgroundColor: "#f6f6f6",
-    padding: 12,
-    borderRadius: 8,
-    fontSize: 16,
-    marginBottom: 15,
-  },
-  textArea: {
-    height: 80,
-    textAlignVertical: "top",
+    alignSelf: "center",
   },
   saveButton: {
     backgroundColor: "#3b82f6",
