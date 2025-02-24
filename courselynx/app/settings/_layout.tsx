78db2c65f54ed039450a7bd20cb69c81112655e3
patwu22 <wu.patrick22@gmail.com>
2025-02-23 23:16:35 -0500

    New folder for settings page

---
+++ courselynx/app/settings/_layout.tsx
@@ -0,0 +1,24 @@
+import Header from "@/components/Header";
+import { Stack, useLocalSearchParams } from "expo-router";
+import { StatusBar } from "react-native";
+
+export default function SettingLayout() {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{
+            header: () => (
+              <Header
+                title="My Profile"
+                hasShare={true}
+              />
+            ),
+          }}
+        />
+      </Stack>
+    </>
+  );
+}
